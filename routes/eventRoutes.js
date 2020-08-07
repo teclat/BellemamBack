02454083c7f3181f9f8d7291f5e2df56945898bd
WallaceGomes <wallacecardosogomes@gmail.com>
2020-08-07 10:56:45 -0300

    chore(images): added image upload middleware

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,12 +4,18 @@ const router = Router();
 const eventController = require('../controllers/EventControllers');
 
 const checkAuth = require('../middleware/check-auth');
+const fileUpload = require('../middleware/file-upload');
+const multer = require('multer');
 
 router.use(checkAuth);
 
-router.post('/:userId/create', eventController.create);
+router.post(
+	'/:userId/create',
+	multer(fileUpload).any(),
+	eventController.create,
+);
 router.get('/:userId', eventController.index);
 router.get('/id/:eventId', eventController.getEventById);
-router.patch('/:eventId/edit', eventController.edit);
+router.patch('/:eventId/edit', multer(fileUpload).any(), eventController.edit);
 
 module.exports = router;
