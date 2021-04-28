fc6e9fc674d7c4bf21c978af752ae1c52fff3c51
WallaceGomes <wallacecardosogomes@gmail.com>
2021-04-28 09:29:17 -0300

    chore: added index products front WP database

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -4,6 +4,9 @@ const HttpError = require('../models/http-error');
 const imageUpload = require('../middleware/image-upload');
 
 exports.index = async (req, res, next) => {
+	let xwpTotalPages;
+	let xwpTotal;
+
 	const config = {
 		params: req.query,
 		auth: {
@@ -15,12 +18,18 @@ exports.index = async (req, res, next) => {
 	const response = await axios
 		.get(`https://bellemam.com.br/wp-json/wc/v3/products`, config)
 		.then((response) => {
+			xwpTotalPages = response.headers['x-wp-totalpages'];
+			xwpTotal = response.headers['x-wp-total'];
 			return response.data;
 		})
 		.catch((err) => {
 			console.log(err);
 			return err;
 		});
+
+	res.setHeader('Access-Control-Expose-Headers', 'x-wp-totalpages');
+	res.setHeader('x-wp-totalpages', xwpTotalPages);
+	res.setHeader('x-wp-total', xwpTotal);
 
 	return res.status(200).json(response);
 };
