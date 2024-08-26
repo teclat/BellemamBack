84cd0df44b704b66e450e2394627553d074db5ed
Raphael3c <phael.ufc@gmail.com>
2024-08-26 03:26:23 -0300

    Corrigindo config database

--- database/config/database.js
+++ database/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 	dialect: 'postgres',
-	host: 'http://srv-captain--j57453ees2',
+	host: 'srv-captain--j57453ees2',
 	username: 'postgres',
 	password: '00c9f3396e895f3b',
 	//password: 'password',
